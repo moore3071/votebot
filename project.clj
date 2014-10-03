a1a3ab8b02cd140a790474bd2bc3feca484df4a2
Jonathan Arnett <jonarnett90@gmail.com>
2014-10-02 21:27:09 -0400

    Added SendGrid library

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [irclj "0.5.0-alpha4"]
-                 [org.clojure/data.json "0.2.5"]]
+                 [org.clojure/data.json "0.2.5"]
+                 [sendgrid "0.1.0"]]
   :main "j3rnbot.core/start")
