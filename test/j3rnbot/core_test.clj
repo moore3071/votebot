37d4d56e59bd3bcd70f7e17ef957a1f785194f3f
Jonathan Arnett <jonarnett90@gmail.com>
2014-10-03 00:17:39 -0400

    Basic parsing tests

--- test/j3rnbot/core_test.clj
+++ test/j3rnbot/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [j3rnbot.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-parse-command
+  (testing "Test command parsing with colon"
+    (is (=
+         (parse-command "j3rnbot: hello")
+         "hello")))
+  (testing "Test command parsing without colon"
+    (is (=
+         (parse-command "j3rnbot hello")
+         "hello"))))
