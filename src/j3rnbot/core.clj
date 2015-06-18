3f47ed136babd210134afcf8025a7c270f8630b7
bam <bam@localhost.localdomain>
2015-06-17 23:12:13 -0400

    added a summary command

--- src/j3rnbot/core.clj
+++ src/j3rnbot/core.clj
@@ -52,6 +52,21 @@
                                        (where {:old false})))))
            "No votes")))
 
+;Compose a string summarizing who voted for what
+(defn rapsheet [irc args]
+  (reply irc args
+         (or
+           (not-empty
+             (reduce-kv
+               #(str %1 "* " %2 ": " (apply str
+                                            (string/join ", "
+                                                         (map :nick %3))) " *")
+               ""
+               (group-by :item (select votes
+                                       (with users)
+                                       (where {:old false})))))
+           "No votes")))
+
 ; Count all of the votes
 (defn count-votes [irc args]
   (reply irc args
@@ -185,6 +200,8 @@
     ".rmvote"
     (rm-vote! irc args sender)
     ".count"
+    ".rapsheet"
+    (rapsheet irc args)
     (count-votes irc args)
     ".whodunnit"
     (whodunnit irc args (get tokens 1))
